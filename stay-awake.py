a4376d34c5898ab07dd90b58cdfb626a102c7692
weibell <13382354+weibell@users.noreply.github.com>
2021-11-22 10:24:53 +0100

    Update log message

--- stay-awake.py
+++ stay-awake.py
@@ -12,7 +12,7 @@ SECONDS_BETWEEN_MOVEMENTS = 60 * 3
 def move():
     pyautogui.move(+1, +1)  # 1 pixel down and to the right
     pyautogui.move(-1, -1)  # 1 pixel up and to the left
-    print("Last mouse movement occured on " + time.ctime())
+    print("Wiggled mouse, current time: " + time.ctime())
 
 
 while(True):
